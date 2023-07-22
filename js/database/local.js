9b7a39a6bb1e9ea2a48ffdb1f90e702a5e533a56
Andy Yang <23339548+a962702@users.noreply.github.com>
2023-07-23 00:15:05 +0800

    Init

---
+++ js/database/local.js
@@ -0,0 +1,60 @@
+class localDB {
+    SQL = null;
+    db = null;
+    /***** Init Database *****/
+    async init() {
+        initSqlJs = window.initSqlJs;
+        this.SQL = await initSqlJs({
+            locateFile: file => 'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.wasm'
+        });
+    }
+
+    /***** Execute Command *****/
+    exec(stm) {
+        this.db.run(stm);
+    }
+
+    /***** Init Database *****/
+    initDB() {
+        this.db = new this.SQL.Database();
+        let command = `
+            CREATE TABLE 'record' (
+                'id' INT(20) NOT NULL,
+                'source' INT NOT NULL,
+                'amount' INT NOT NULL,
+                'commit' TEXT(50),
+                PRIMARY KEY ('id')
+            );
+            CREATE TABLE 'overview' (
+                'name' TEXT(50) NOT NULL,
+                'updated_at' DATETIME NOT NULL
+            );
+            CREATE TABLE 'personnel' (
+                'id' INT(20) NOT NULL,
+                'name' INT(20) NOT NULL,
+                PRIMARY KEY ('id')
+            );`
+        db.run(command);
+    }
+
+    /***** Load Database *****/
+    load() {
+        const binaryArray = localStorage.getItem("AIOMS_SqlDB");
+        this.db = new this.SQL.Database(binaryArray);
+    }
+
+    /***** Save Database *****/
+    save() {
+        const binaryArray = this.db.export();
+        localStorage.setItem("AIOMS_SqlDB", binaryArray);
+    }
+
+    /***** Get DB binaryArray *****/
+    get_binaryArray(){
+        return this.db.export();
+    }
+}
+
+export {
+    localDB
+}
---
+++ js/database/main.js
@@ -0,0 +1,59 @@
+// Layer for generic database access
+import { localDB } from 'js/database/local.js';
+
+class database {
+    localDB = null;
+    GDDB = null;
+
+    async init() {
+        obj_localDB = new localDB();
+        if (!localStorage.getItem("AIOMS_INITDB")){
+            // TODO
+        }
+    }
+
+    // Get available storage
+    get_available_storage(){
+        return Array('local', 'GD');
+    }
+
+    // Get setup storage
+    get_setup_storage(){
+        return Array('local');
+    }
+
+    // Execute SQL statement and return data
+    exec(stm){
+        arr = Array();
+        if(this.localDB != null){
+            arr['status'] = 'OK';
+            arr['result'] = this.localDB.exec(stm);
+            return arr;
+        }
+        arr['status'] = 'ERROR_UNINITIALIZED';
+        return arr;
+    }
+
+    // Save all DB
+    save(){
+        setup_storage = localStorage.getItem("AIOMS_STORAGE");
+        // TODO
+    }
+
+    // Export DB
+    export(){
+        arraybuff = this.localDB.get_binaryArray();
+        blob = new Blob([arraybuff]);
+        a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        a.href = window.URL.createObjectURL(blob);
+        a.download = "sql.db";
+        a.onclick = function () {
+			setTimeout(function () {
+				window.URL.revokeObjectURL(a.href);
+			}, 1000);
+		};
+		a.click();
+    }
+}

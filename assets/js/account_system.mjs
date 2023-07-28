8787072dfc276e6d255990046562295e7dab863e
Andy Yang <23339548+a962702@users.noreply.github.com>
2023-07-28 14:32:01 +0800

    fix

--- assets/js/account_system.mjs
+++ assets/js/account_system.mjs
@@ -12,7 +12,7 @@ $(function () {
         ov.addLog($("#overview_modal_add_date").val(), $("#overview_modal_add_source").val(), $("#overview_modal_add_amount").val(), $("#overview_modal_add_commit").val());
         dialog.dialog("close");
     };
-    dialog = $("#dialog-form").dialog({
+    let dialog = $("#dialog-form").dialog({
         autoOpen: false,
         height: 400,
         width: 350,

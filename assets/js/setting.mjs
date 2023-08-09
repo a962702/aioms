bdc00bb12b228f8d8fc3039f4275a3118bc90b11
Andy Yang <23339548+a962702@users.noreply.github.com>
2023-08-09 12:58:45 +0800

    update

--- assets/js/setting.mjs
+++ assets/js/setting.mjs
@@ -5,6 +5,26 @@ let db = new database();
 await db.init();
 
 let s = new setting(db);
-document.getElementById("setting_btn_runcommand").addEventListener('click', ()=>{
+$("#setting_btn_runcommand").on('click', ()=>{
     s.exec(document.getElementById('sql_statement').value);
 });
+
+$("#btn_GDDB_auth").on("click", async () => {
+    if (db.GD_auth()) {
+        document.getElementById('signout_button').style.visibility = 'visible';
+        document.getElementById('authorize_button').innerText = 'Refresh';
+        await db.GD_list();
+    }
+})
+
+$("#btn_GDDB_signout").on("click", () => {
+    db.GD_signout();
+})
+
+$("#btn_GDDB_load").on("click", () => {
+    db.GD_load();
+})
+
+$("#btn_GDDB_save").on("click", () => {
+    db.GD_save();
+})
